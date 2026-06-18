0810095981c40b7b0c082d3e75a91606e8a591a6
McWooden <huddin8876@gmail.com>
2026-06-18 20:23:42 +0700

    it work with removable keystatic ah

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Add any custom configuration here if needed
+  outputFileTracingIncludes: {
+    '/api/keystatic/[...params]': ['./src/content/**/*'],
+    '/*': ['./src/content/**/*'],
+  },
 };
 
 export default nextConfig;
